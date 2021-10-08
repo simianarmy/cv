bed672afcf3734671d33e9a6feb6d7e54228e793
Marc Mauger <simianarmy@gmail.com>
2021-10-08 19:15:37 -0400

    updated with personal info

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteTitle: 'Gatsby Starter Resume', // <title>
+  siteTitle: 'Marc Mauger Resume', // <title>
   manifestName: 'Resume',
   manifestShortName: 'Landing', // max 12 characters
   manifestStartUrl: '/',
@@ -7,33 +7,28 @@ module.exports = {
   manifestThemeColor: '#663399',
   manifestDisplay: 'standalone',
   manifestIcon: 'src/assets/img/website-icon.png',
-  pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
-  firstName: 'Anubhav',
-  lastName: 'Srivastava',
+  pathPrefix: `/cv/`, // This path is subpath of your hosting https://domain/portfolio
+  firstName: 'Marc',
+  lastName: 'Mauger',
   // social
   socialLinks: [
     {
       icon: 'fa-github',
       name: 'Github',
-      url: 'https://github.com/anubhavsrivastava',
+      url: 'https://github.com/simianarmy',
     },
     {
       icon: 'fa-linkedin-in',
       name: 'Linkedin',
-      url: 'https://linkedin.com/in/theanubhav/',
+      url: 'https://linkedin.com/in/🧽-marc-mauger-6664988',
     },
     {
       icon: 'fa-twitter',
       name: 'Twitter',
-      url: 'https://twitter.com/onlyanubhav',
-    },
-    {
-      icon: 'fa-facebook-f',
-      name: 'Facebook',
-      url: 'https://facebook.com/theanubhav',
+      url: 'https://twitter.com/simianarmy',
     },
   ],
-  email: 'anubhav.srivastava00@gmail.com',
-  phone: '000-00000',
-  address: '305 Del Sol, Sunnyvale, CA',
+  email: 'simianarmy@gmail.com',
+  phone: '206-953-4721',
+  address: 'Seattle, WA',
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,12 +22,7 @@ const IndexPage = () => (
             {config.address} · {config.phone} ·
             <a href={`mailto:${config.email}`}>{config.email}</a>
           </div>
-          <p className="lead mb-5">
-            I am experienced in leveraging agile frameworks to provide a robust
-            synopsis for high level overviews. Iterative approaches to corporate
-            strategy foster collaborative thinking to further the overall value
-            proposition.
-          </p>
+          <p className="lead mb-5">Lorem Ipsum dolores</p>
           <div className="social-icons">
             {config.socialLinks.map(social => {
               const { icon, url } = social;
@@ -53,32 +48,19 @@ const IndexPage = () => (
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
               <h3 className="mb-0">Senior Web Developer</h3>
-              <div className="subheading mb-3">Intelitec Solutions</div>
-              <p>
-                Bring to the table win-win survival strategies to ensure
-                proactive domination. At the end of the day, going forward, a
-                new normal that has evolved from generation X is on the runway
-                heading towards a streamlined cloud solution. User generated
-                content in real-time will have multiple touchpoints for
-                offshoring.
-              </p>
+              <div className="subheading mb-3">Zwift</div>
+              <p>Lorem Ipsum</p>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">March 2013 - Present</span>
+              <span className="text-primary">April 2018 - April 2021</span>
             </div>
           </div>
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">Web Developer</h3>
-              <div className="subheading mb-3">Intelitec Solutions</div>
-              <p>
-                Capitalize on low hanging fruit to identify a ballpark value
-                added activity to beta test. Override the digital divide with
-                additional clickthroughs from DevOps. Nanotechnology immersion
-                along the information highway will close the loop on focusing
-                solely on the bottom line.
-              </p>
+              <h3 className="mb-0">Software Manager</h3>
+              <div className="subheading mb-3">Double Down Interactive</div>
+              <p>Lorem Ipsum</p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">December 2011 - March 2013</span>
@@ -87,15 +69,9 @@ const IndexPage = () => (
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">Junior Web Designer</h3>
-              <div className="subheading mb-3">Shout! Media Productions</div>
-              <p>
-                Podcasting operational change management inside of workflows to
-                establish a framework. Taking seamless key performance
-                indicators offline to maximise the long tail. Keeping your eye
-                on the ball while performing a deep dive on the start-up
-                mentality to derive convergence on cross-platform integration.
-              </p>
+              <h3 className="mb-0">Senior Web Developer</h3>
+              <div className="subheading mb-3">Cricket Moon Media</div>
+              <p>Lorem Ipsum</p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">July 2010 - December 2011</span>
@@ -104,14 +80,9 @@ const IndexPage = () => (
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
             <div className="resume-content">
-              <h3 className="mb-0">Web Design Intern</h3>
-              <div className="subheading mb-3">Shout! Media Productions</div>
-              <p>
-                Collaboratively administrate empowered markets via plug-and-play
-                networks. Dynamically procrastinate B2C users after installed
-                base benefits. Dramatically visualize customer directed
-                convergence without revolutionary ROI.
-              </p>
+              <h3 className="mb-0">Owner</h3>
+              <div className="subheading mb-3">Simian Labs</div>
+              <p>Lorem Ipsum</p>
             </div>
             <div className="resume-date text-md-right">
               <span className="text-primary">September 2008 - June 2010</span>
@@ -131,24 +102,12 @@ const IndexPage = () => (
 
           <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
             <div className="resume-content">
-              <h3 className="mb-0">University of Colorado Boulder</h3>
+              <h3 className="mb-0">University of Washington</h3>
               <div className="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 3.23</p>
+              <div>Computer Science</div>
             </div>
             <div className="resume-date text-md-right">
-              <span className="text-primary">August 2006 - May 2010</span>
-            </div>
-          </div>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
-            <div className="resume-content">
-              <h3 className="mb-0">James Buchanan High School</h3>
-              <div className="subheading mb-3">Technology Magnet Program</div>
-              <p>GPA: 3.56</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2002 - May 2006</span>
+              <span className="text-primary">1995 - 2014</span>
             </div>
           </div>
         </div>
@@ -164,44 +123,40 @@ const IndexPage = () => (
           <h2 className="mb-5">Skills</h2>
 
           <div className="subheading mb-3">
-            Programming Languages &amp; Tools
+            Programming Languages
           </div>
-          <ul className="list-inline dev-icons">
-            <li className="list-inline-item">
-              <i className="fab fa-html5"></i>
+          <ul className="fa-ul">
+            <li>
+              Backend
+              (Node, Python, Ruby, PHP, Java, Scala, SQL)
             </li>
-            <li className="list-inline-item">
-              <i className="fab fa-css3-alt"></i>
+            <li>
+              Frontend
+              (JS, Elm, CSS3, WebGL)
             </li>
-            <li className="list-inline-item">
-              <i className="fab fa-js-square"></i>
+          </ul>
+
+          <div className="subheading mb-3">
+            Tools
+          </div>
+          <ul className="fa-ul">
+            <li>git, zsh, tmux, vim, yarn, npm, Webpack, Docker, K8s, redis, nginx</li>
+          </ul>
+
+          <div className="subheading mb-3">
+            Frameworks &amp; Libraries
+          </div>
+          <ul className="fa-ul">
+            <li>
+              Web (NextJS, React, Angular, Bootstrap, Redux, D3.js, RxJS, Ruby On Rails, Express, testing-library, TestCafe)
             </li>
-            <li className="list-inline-item">
-              <i className="fab fa-angular"></i>
+            <li>
+              UI
+              (Figma, Storybook)
             </li>
-            <li className="list-inline-item">
-              <i className="fab fa-react"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-node-js"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-sass"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-less"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-wordpress"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-gulp"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-grunt"></i>
-            </li>
-            <li className="list-inline-item">
-              <i className="fab fa-npm"></i>
+            <li>
+              Build
+                (Webpack, Jenkins, Buildbot, Docker, Kubernetes, AWS, Linux)
             </li>
           </ul>
 
@@ -214,6 +169,10 @@ const IndexPage = () => (
             <li>
               <i className="fa-li fa fa-check"></i>
               Cross Browser Testing &amp; Debugging
+            </li>
+            <li>
+              <i className="fa-li fa fa-check"></i>
+              TDD, Unit & Integration tests
             </li>
             <li>
               <i className="fa-li fa fa-check"></i>
@@ -236,16 +195,12 @@ const IndexPage = () => (
         <div className="w-100">
           <h2 className="mb-5">Interests</h2>
           <p>
-            Apart from being a web developer, I enjoy most of my time being
-            outdoors. In the winter, I am an avid skier and novice ice climber.
-            During the warmer months here in Colorado, I enjoy mountain biking,
-            free climbing, and kayaking.
+            My passion is spending time outdoors, especially at the higher altitudes.
+            In my free time I enjoy backpacking, rock climbing (trad), and bouldering.
+            I will occasionally attempt some alpine climbing & can be talked into snowboarding.
           </p>
           <p className="mb-0">
-            When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
+            When at home I enjoy cooking, gardening, live music, & sci-fi novels.
           </p>
         </div>
       </section>
@@ -259,37 +214,7 @@ const IndexPage = () => (
         <div className="w-100">
           <h2 className="mb-5">Awards &amp; Certifications</h2>
           <ul className="fa-ul mb-0">
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Google Analytics Certified Developer
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>
-              Mobile Web Specialist - Google Certification
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2009
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - University of Colorado Boulder - Adobe Creative Jam 2008
-              (UI Design Category)
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>2<sup>nd</sup>
-              Place - University of Colorado Boulder - Emerging Tech Competition
-              2008
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>1<sup>st</sup>
-              Place - James Buchanan High School - Hackathon 2006
-            </li>
-            <li>
-              <i className="fa-li fa fa-trophy text-warning"></i>3<sup>rd</sup>
-              Place - James Buchanan High School - Hackathon 2005
-            </li>
+            Maybe one day...
           </ul>
         </div>
       </section>
